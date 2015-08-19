b43977a3eb2cb4b143334c73bbbb0b6f20f8cc42
Ivan <liunianfengy@163.com>
2015-08-19 19:29:11 +0800

    add static

---
+++ public/pagejs/statics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+	  var hm = document.createElement("script");
+	    hm.src = "//hm.baidu.com/hm.js?5c887b7aceaa247b80ba1e89c78193a0";
+	      var s = document.getElementsByTagName("script")[0]; 
+	        s.parentNode.insertBefore(hm, s);
+})();
